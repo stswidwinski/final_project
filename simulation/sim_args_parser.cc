d04d895bda4d11cb566161a1d3bb162f9be0235c
Stan <stanislaw.swidwinski@yale.edu>
2016-11-02 19:42:53 -0400

    Add argument parser for the simulation

---
+++ simulation/sim_args_parser.cc
@@ -0,0 +1,92 @@
+#include "simulation/sim_args_parser.h"
+
+#include <sstream>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+template <typename T>
+T parse_param(char*& string_param) {
+  std::stringstream conv;
+  T converted;
+  conv << string_param;
+  conv >> converted;
+  return converted;
+};
+
+std::set<Model> get_models(int argc, char** argv) {
+  std::set<Model> res;
+  for (int i = 15; i < argc; i++) {
+    if (strcmp(argv[i], "sequential") == 0) {
+      res.insert(Model::sequential);
+    } else if (strcmp(argv[i], "real_time") == 0) {
+      res.insert(Model::real_time);
+    } else if (strcmp(argv[i], "batched") == 0) {
+      res.insert(Model::batched);
+    }
+  }
+
+  if (res.size() == 0) {
+    std::cerr << "ERROR. No models specified\n";
+    exit(1);
+  }
+
+  return res;
+}
+
+std::set<Data> get_req_data(int argc, char** argv) {
+  std::set<Data> res;
+  for (int i = 15; i < argc; i++) {
+    if (strcmp(argv[i], "load") == 0) {
+      res.insert(Data::load);
+    } else if (strcmp(argv[i], "avg_proc_time") == 0) {
+      res.insert(Data::avg_proc_time);
+    } else if (strcmp(argv[i], "std_dev_proc_time") == 0) {
+      res.insert(Data::std_dev_proc_time);
+    } else if (strcmp(argv[i], "locks_in_time") == 0) {
+      res.insert(Data::locks_in_time);
+    }
+  }
+
+  if (res.size() == 0) {
+    std::cerr << "ERROR. No data requested\n";
+    exit(1);
+  }
+
+  return res;
+}
+
+SimulationArgs parse_arguments(int argc, char** argv) {
+  if (argc < 17) {
+    std::cerr << "ERROR. Too few arguments for the generator\n";
+    exit(1);
+  }
+
+  SimulationArgs sa;
+  sa.time_period = parse_param<unsigned int>(argv[2]);
+  sa.txn_gen_params = {
+    .lock_time_multiplier = parse_param<unsigned int>(argv[1]),
+    .txn_number = parse_param<unsigned int>(argv[3]),
+    .time_period = sa.time_period,
+    .cont_lock = {
+      .lock_space = parse_param<unsigned int>(argv[7]),
+      parse_param<unsigned int>(argv[8]),
+      parse_param<unsigned int>(argv[9])
+    },
+    .uncont_lock = {
+      .lock_space = parse_param<unsigned int>(argv[4]),
+      parse_param<unsigned int>(argv[5]),
+      parse_param<unsigned int>(argv[6])
+    },
+    .write_txns_perc = parse_param<double>(argv[10]),
+    .seed_chance = parse_param<double>(argv[11]),
+    .linear_factor = parse_param<unsigned int>(argv[12]) 
+  };
+  sa.batch_length = parse_param<unsigned int>(argv[13]);
+  sa.dump_path_root = argv[14];
+  sa.reps = parse_param<unsigned int>(argv[15]);
+  sa.requested_models = get_models(argc, argv);
+  sa.requested_data = get_req_data(argc, argv);
+
+  return sa;
+}
---
+++ simulation/sim_args_parser.h
@@ -0,0 +1,61 @@
+#ifndef _SIM_ARGS_PARSER_H_
+#define _SIM_ARGS_PARSER_H_
+
+#include "simulation/txn_generator.h"
+
+#include <set>
+
+// the following are the legal models
+enum class Model {
+  sequential,
+  real_time,
+  batched,
+  count
+};
+
+// the following are the legal data dump requests
+enum class Data {
+  load,
+  avg_proc_time,
+  std_dev_proc_time,
+  locks_in_time
+};
+
+struct SimulationArgs {
+  TxnGeneratorParams txn_gen_params;
+  unsigned int batch_length;
+  unsigned int time_period;
+  unsigned int reps;
+  std::string dump_path_root;
+  std::set<Model> requested_models;
+  std::set<Data> requested_data;
+};
+
+/*
+ *  The arguments are given in the following order:
+ *    1) The workload creation parameters -- details within txn_generator.h
+ *       a) lock_time_multiplier (unsigned int)   1)
+ *       b) time_period          (unsigned int)   2)
+ *       c) transaction number   (unsigned int)   3)
+ *       d) uncontested locks
+ *          i) Lock Space Size   (unsigned int)   4)
+ *          ii) Avg # held       (unsigned int)   5)
+ *          iii) Held # std dev  (unsigned int)   6)
+ *       e) contested locks
+ *          i) Lock Space Size   (unsigned int)   7)
+ *          ii) Avg # held       (unsigned int)   8)
+ *          iii) Held # std dev  (unsigned int)   9)
+ *       f) % of write txns      (double)         10)
+ *       g) bursty seed chance   (double)         11)
+ *       h) bursty linear factor (unsigned int)   12)
+ *    2) Batch Length (unsigned int)              13)
+ *    3) Output file dir                          14)
+ *    4) Repetitions of experiments               15)
+ *  The following two are given as strings after the former. Any number of either
+ *  can be given, but at least one of each must be present.
+ *    5) The models requested                     16 - *)
+ *    6) The data dumps requested                 *) - *)
+ */
+SimulationArgs parse_arguments(int argc, char** argv);
+
+#endif //_SIM_ARGS_PARSER_H_
