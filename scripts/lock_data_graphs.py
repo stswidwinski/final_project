76c93798501aa2927fd43b888fd0616e462839ed
Stan <stanislaw.swidwinski@yale.edu>
2016-11-05 01:12:07 -0400

    rewritten for new utils and using less memory

--- scripts/lock_data_graphs.py
+++ scripts/lock_data_graphs.py
@@ -5,69 +5,108 @@ import pandas
 import numpy as np
 from script_utils.dir_utils import *
 
-def aggregate_data_for_directory(path):
-    # For every lock data file in the dir, make a plot
-    all_files = os.listdir(path)
-    relevant_files = [a for a in all_files if a.find('locks_in_time') != -1]
-
-    # Find the maximal index on any of those files
-    max_index = 0
-    for file in relevant_files:
-        index = int(file[file.rfind('_') + 1:])
-        if index > max_index:
-             max_index = index
+class LockDataRepetition:
+    index = 0
+    data_file_paths = []
     
-    # Create pandas data frames for all of these
-    dfs = []
-    for index in range(0, max_index + 1):
-        wanted_files = [a for a in relevant_files if a.endswith('_' + str(index))]
-        print(wanted_files) 
-        # each file corresponds to a different model
-        series = []
-        for file in wanted_files:
-            model = file[:file.find("_locks_in_time")]
-            file_path = os.path.join(path, file)
-            data = pandas.read_csv(file_path)
-            series.append([model, data['number_of_locks']])
-        
-        data_frame = pandas.DataFrame()
-        for serie in series:
-            data_frame[serie[0]] = serie[1]
-        
-        dfs.append(
-            { 
-                'index': index,
-                'df': data_frame
-            })
+    def __init__(self):
+        self.index = 0
+        self.data_file_paths = []
 
-    return dfs
+class MeasLockData:
+    name = ''
+    # an array of lock data repetitions
+    reps = []
+
+    def __init__(self):
+        self.name = ''
+        self.reps = []
+
+def aggregate_metadata(raw_data_path):
+    all_data_file_paths = get_data_paths(raw_data_path, "", "txns_in_time");
+
+    # An array of MeasLockData.
+    data_metadata = []
+    for meas_data in all_data_file_paths:
+        meas_metadata = MeasLockData()
+        meas_metadata.name = meas_data['meas_name']
+        
+        meas_files = meas_data['meas_data_files_paths']
+        max_index = 0
+        for path in meas_files:
+            index = int(path[path.rfind('_') + 1:])
+            if index > max_index:
+                max_index = index
+
+        for index in range(0, max_index + 1):
+            rep_meta = LockDataRepetition()
+            rep_meta.data_file_paths = [a for a in meas_files if a.endswith('_' + str(index))]
+            rep_meta.index = index
+            meas_metadata.reps.append(rep_meta)
+        data_metadata.append(meas_metadata)
+    return data_metadata
+
+def get_rep_data(meas_data_path, rep_meta_data):
+    series = []
+    for rep_file_path in rep_meta_data.data_file_paths:
+        # each rep is a different model
+        model = rep_file_path[
+            rep_file_path.rfind('/') + 1 :
+            rep_file_path.rfind("_txns_in_time")]
+        local_data = pandas.read_csv(rep_file_path)
+        series.append([model, local_data['number_of_txns']])
+       
+    # sort longest to shortest to overcome limitations of pandas.insert
+    series = sorted(series, key=lambda serie: len(serie[1]), reverse=True)
+    data_frame = pandas.DataFrame()
+    for serie in series:
+        data_frame.insert(len(data_frame.columns.tolist()), serie[0], serie[1])
+
+    return data_frame    
 
 def create(path_to_raw_data, path_to_proc_data):
     # make the graphs!
-    all_dirs = os.listdir(path_to_raw_data)
-    for dir in all_dirs:
-        save_path = os.path.join(path_to_proc_data, dir, "lock_graphs")
+    metadata = aggregate_metadata(path_to_raw_data)
+    for mdata in metadata:
+        # We are considering the MeasLockData elements
+        save_path = os.path.join(path_to_proc_data, mdata.name, "lock_graphs")
         create_dir(save_path)
+        meas_path = os.path.join(path_to_raw_data, mdata.name)
 
-        dir_path = os.path.join(path_to_raw_data, dir);
-        data = aggregate_data_for_directory(dir_path)
-        for df_elt in data:
+        for rep in mdata.reps:
+            rep_data = get_rep_data(meas_path, rep)
+            
+            # scatter plot
             ax = plt.figure(0).add_subplot(111)
             plt.title("Concurrent txns in time")
             plt.xlabel("Time")
             plt.ylabel("Number of transactions in flight")
 
-            columns = df_elt['df'].columns.tolist()
-            print(columns)
+            columns = sorted(rep_data.columns.tolist(), key=lambda column: len(rep_data[column].dropna().tolist()), reverse=True)
             colors = cm.rainbow(np.linspace(0, 1, len(columns)))
             for column, color in zip(columns, colors):
-                data = df_elt['df'][column].dropna()
+                data = rep_data[column].dropna()
                 ax.scatter(
                         data.index.tolist(),
                         data.tolist(),
                         label = column,
                         color = color)
             plt.legend()
-            plt.savefig(os.path.join(save_path, "concur_txns_" + str(df_elt['index'])))
+            plt.savefig(os.path.join(save_path, "concur_txns_" + str(rep.index)))
+            plt.cla()
+            plt.clf()
+
+            # histogram plot
+            ax = plt.figure(0).add_subplot(111)
+            plt.title("Concurrent txns during execution")
+            plt.xlabel("Concurrent txns")
+            plt.ylabel("Aggregated time")
+
+            colors = cm.rainbow(np.linspace(0, 1, len(columns)))
+            for column, color in zip(columns, colors):
+                data = rep_data[column].dropna()
+                ax.hist(data, 100, alpha=0.5, label=column)
+            plt.legend()
+            plt.savefig(os.path.join(save_path, "concur_txns_hist_" + str(rep.index)))
             plt.cla()
             plt.clf()
