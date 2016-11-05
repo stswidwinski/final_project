d160717f4a1624a80194851faae04635822146ee
Stan <stanislaw.swidwinski@yale.edu>
2016-11-05 01:13:02 -0400

    changed to use the new dirutils

--- scripts/batch_length_graphs.py
+++ scripts/batch_length_graphs.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import os
 import pandas
+from script_utils.dir_utils import *
 import numpy as np
 
 def aggregate_data(path_to_raw_data):
     # from all the subdirectories grab the necessary pieces of data ...
-    dirs = os.listdir(path_to_raw_data)
-    relevant_dirs = [a for a in dirs if a.find('batch_length') != -1]
+    rel_data = get_data_paths(path_to_raw_data, 'batch_length', 'avg_std')
 
     comp_time_data = pandas.DataFrame(
             {
                 'batch_length':[], 
-                'avg_comp_time':[], 
+                'total_time':[], 
                 'model':[]
             });
     avg_txn_comp_data = pandas.DataFrame(
@@ -23,43 +23,37 @@ def aggregate_data(path_to_raw_data):
                 'model': []
             })
     # aggregate the data...
-    for dir in relevant_dirs:
-        base_dir = os.path.join(path_to_raw_data, dir);
-        batch_length = int(dir[dir.rfind('_') + 1 : ])
+    for meas_data in rel_data:
+        meas_dir = os.path.join(path_to_raw_data, meas_data['meas_name']);
+        batch_length = int(meas_dir[meas_dir.rfind('_') + 1 : ])
 
-        # get the batched completion times
-        local_dirs = os.listdir(os.path.join(path_to_raw_data, dir))
-        bastd = [a for a in local_dirs if a.find('batched_avg_std') != -1]
-        if len(bastd) != 0:
-            batched_comp_times = pandas.read_csv(os.path.join(base_dir, bastd[0]));
-            comp_time_data = comp_time_data.append(
-                    {
-                        'batch_length': batch_length, 
-                        'avg_comp_time': np.mean(batched_comp_times['total_time']),
-                        'model': "batched",
-                    }, ignore_index = True)
-            avg_txn_comp_data = avg_txn_comp_data.append(
-                    {
-                        'batch_length': batch_length,
-                        'avg_txns_comp_time': np.mean(
-                            batched_comp_times['average_completion_time']),
-                        'avg_std_dev': np.mean(
-                            batched_comp_times['standard_deviation']),
-                        'model': "batched",
-                    }, ignore_index = True)
-        else:
-            print("Error creating batch length graphs... files not found.")
-
-        #get the non-batched data points...
-        other_astd = [
-                a for a in local_dirs if a.find('avg_std') != -1 and a.find('batched') == -1]
-        for other_dir in other_astd:
-            model = other_dir[:other_dir.find('_avg_std')]
-            data = pandas.read_csv(os.path.join(base_dir, other_dir))
+        for data_file_path in meas_data['meas_data_files_paths']:
+#            if data_file_path.rfind('_batched_') != -1:
+#                batched_comp_times = pandas.read_csv(data_file_path);
+#                comp_time_data = comp_time_data.append(
+#                        {
+#                            'batch_length': batch_length, 
+#                            'avg_comp_time': np.mean(batched_comp_times['total_time']),
+#                            'model': "batched",
+#                        }, ignore_index = True)
+#                avg_txn_comp_data = avg_txn_comp_data.append(
+#                        {
+#                            'batch_length': batch_length,
+#                            'avg_txns_comp_time': np.mean(
+#                                batched_comp_times['average_completion_time']),
+#                            'avg_std_dev': np.mean(
+#                                batched_comp_times['standard_deviation']),
+#                            'model': "batched",
+#                        }, ignore_index = True)
+#            else:
+            model = data_file_path[
+                    data_file_path.rfind('/') + 1 : 
+                    data_file_path.rfind('_avg_std')]
+            data = pandas.read_csv(data_file_path)
             comp_time_data = comp_time_data.append(
                 {
                     'batch_length': batch_length,
-                    'avg_comp_time': np.mean(data['total_time']),
+                    'total_time': np.mean(data['total_time']),
                     'model': model
                 }, ignore_index = True)
             avg_txn_comp_data = avg_txn_comp_data.append(
@@ -77,7 +71,7 @@ def create(path_to_raw_data, path_to_proc_data):
 
     # Create the completion time graphs first
     ax = plt.figure().add_subplot(111)
-    plt.title("Workload completion time as a function of batch length")
+    plt.title("Workload completion time as a function of batch length", y = 1.03)
     plt.xlabel("Batch length")
     plt.ylabel("Completion time")
 
@@ -86,7 +80,7 @@ def create(path_to_raw_data, path_to_proc_data):
     for model, color in zip(models, colors):
         ax.scatter(
             comp_time[comp_time['model'] == model]["batch_length"].tolist(),
-            comp_time[comp_time['model'] == model]["avg_comp_time"].tolist(),
+            comp_time[comp_time['model'] == model]["total_time"].tolist(),
             label = model,
             color = color)
     plt.legend()
@@ -96,7 +90,7 @@ def create(path_to_raw_data, path_to_proc_data):
 
     # Create the average transaction length graph
     ax = plt.figure(0).add_subplot(111)
-    plt.title("Average transaction completion time as a function of batch length")
+    plt.title("Average transaction completion time as a function of batch length", y = 1.03)
     plt.xlabel("Batch length")
     plt.ylabel("Avg completion time")
 
